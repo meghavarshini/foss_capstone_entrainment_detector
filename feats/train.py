68754f6d2e40e14f6d75ff353e8fffba940e179e
Meghavarshini Krishnaswamy <meghavarshini@gmail.com>
2023-03-29 22:20:36 -0700

    Update train.py

--- feats/train.py
+++ feats/train.py
@@ -5,10 +5,10 @@ from ecdc import *
 def make_argument_parser():
 	parser = argparse.ArgumentParser(description='VAE MNIST Example')
 	parser.add_argument('--model_name', type=str,
-						default= "/home/tomcat/entrainment/feat_files/baseline_1_models/trained_VAE_nonorm_nopre_l1.pt",
+						default= "./baseline_1_models/trained_VAE_nonorm_nopre_l1.pt",
 						help="name of model file")
 	parser.add_argument('--h5_directory', type=str,
-						default="/home/tomcat/entrainment/feat_files/baseline_1_h5",
+						default="./baseline_1_h5",
 						help='location of h5 files')
 	parser.add_argument('--batch-size', type=int, default=128, metavar='N',
 						help='input batch size for training (default: 128)')
@@ -35,11 +35,6 @@ def model_setup(model_name, seed, cuda, data_directory):
 	torch.manual_seed(seed)
 	if cuda:
 		torch.cuda.manual_seed(seed)
-
-# kwargs = {'num_workers': 1, 'pin_memory': True} if args.cuda else {}
-
-# f = h5py.File('interaction_Fisherfeats.h5','r')
-# dataset=f['dataset']
 
 
 	fdset = EntDataset(data_directory + "/" + "train_Fisher_nonorm.h5")
@@ -86,8 +81,7 @@ def train(each_epoch, model, train_loader, optimizer, cuda):
 
 	return train_loss
 
-#Lines 88,89 depreciated
-# https://stackoverflow.com/questions/61720460/volatile-was-removed-and-now-had-no-effect-use-with-torch-no-grad-instread
+
 def validate(model, val_loader, cuda):
 	model.eval()
 	val_loss = 0
@@ -95,13 +89,11 @@ def validate(model, val_loader, cuda):
 		if cuda:
 			data = data.cuda()
 			y_data = y_data.cuda()
-		# data = Variable(data, volatile=True)
-		# y_data = Variable(y_data, volatile=True)
+		
 		data = Variable(data)
 		y_data = Variable(y_data)
 		recon_batch = model(data)
 		val_loss += loss_function(recon_batch, y_data).data
-		# val_loss += loss_function(recon_batch, y_data).data[0]
 
 	val_loss /= len(val_loader.dataset)
 	print(('====> Validation set loss: {:.4f}'.format(val_loss)))
@@ -123,10 +115,7 @@ if __name__ == "__main__":
 	baseline_model, baseline_optimizer, baseline_train_loader, baseline_val_loader = \
 		model_setup(args.model_name, args.seed, args.cuda, args.h5_directory)
 
-# for epoch in range(1, 3):
-#Notes- torch.save saves both the state dict as well as the optimizer-
-# if we have a model and all we want to do is use it, then we save the state dict. But if we want
-# further train, fine-tune, then we need both he optimizer as well as the state dict.
+
 	for epoch in range(1, args.epochs + 1):
 		tloss = train(each_epoch = epoch, model = baseline_model,
 					  train_loader= baseline_train_loader,
@@ -141,6 +130,4 @@ if __name__ == "__main__":
 			best_epoch = epoch
 			print("epoch: ", vloss, "epoch: ", epoch)
 			torch.save(baseline_model, args.model_name)
-			#model = torch.load(PATH)
-			#model.eval()
-			#parameters- check they are non-empty
+			
